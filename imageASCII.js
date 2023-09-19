5e5025d60e4ac9f53ecc330362f4b812d4c192bf
Erik Matos Freitas <erikmfa190230@gmail.com>
2023-09-19 20:37:57 -0300

    getting image data and matching pixels with ASCII code

--- imageASCII.js
+++ imageASCII.js
@@ -1,3 +1,46 @@
-async function generateASCII (){
-    
+const Jimp = require('jimp');
+
+function convertToAscii(imageBuffer) {
+    return new Promise (async (resolve, reject) => {
+
+        try {
+            const image = await Jimp.read(imageBuffer);
+
+            // Resize the image to a suitable width
+            const width = 80; // Adjust the width as needed
+            image.resize(width, Jimp.AUTO);
+
+            let asciiArt = '';
+
+            // Define the ASCII characters to use
+            const asciiChars = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.'];
+
+            // Loop through each pixel in the resized image and map it to an ASCII character
+            image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+                const red = image.bitmap.data[idx];
+                const green = image.bitmap.data[idx + 1];
+                const blue = image.bitmap.data[idx + 2];
+
+                const grayscale = (red + green + blue) / 3;
+
+                const asciiIndex = Math.floor((grayscale / 255) * (asciiChars.length - 1));
+                asciiArt += asciiChars[asciiIndex];
+
+                if (x === image.bitmap.width - 1) {
+                    asciiArt += '\n'; // Start a new line for each row
+                }
+            });
+
+            resolve (asciiArt);
+        } catch (error) {
+            console.error('Error converting image to ASCII:', error);
+            reject(error); ''; // Return an empty string on error
+        };
+    )};
+
 }
+
+
+    
+
+module.exports = convertToAscii;
--- server.js
+++ server.js
@@ -1,17 +1,37 @@
-const express = require("express");
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const convertToAscii = require('./imageASCII');
+const Jimp = require('jimp');
+
 const app = express();
-const upload = require('./public/multer');
 
-app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public'));
 
-app.get('/', (req,res) => {
-    res.send('Test')
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.get('/', (req, res) => {
+    res.render('index', { asciiArt: '' });
 });
 
-app.post('/upload', upload.single("image") , (req,res) => {
-    res.status(500)
+app.post('/upload', upload.single('image'), async (req, res) => {
+    if (!req.file) {
+        return res.redirect('/');
+    }
+
+    try {
+        const image = await Jimp.read(req.file.buffer);
+        const asciiArt = convertToAscii(image);
+
+        res.render('index', { asciiArt });
+    } catch (error) {
+        console.error(error);
+        res.redirect('/');
+    }
 });
 
 app.listen(3000, () => {
-    console.log("Online")
+    console.log(`Server is running on port 3000`);
 });
