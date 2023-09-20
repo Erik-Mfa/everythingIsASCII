9a5ee96a4d19e04dcd17f8e14f28e4550e8eaf0d
Erik Matos Freitas <erikmfa190230@gmail.com>
2023-09-19 22:04:42 -0300

    remove promise

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # everythingIsASCII
-the objective of this repo is try to follow the idea of creating ASCII images and videos from the original one 
+the objective of this repo is try to follow the idea of creating ASCII art from images and videos
+
+original idea: https://youtu.be/55iwMYv8tGI?si=iP8hGDmJDPS__qFp
--- imageASCII.js
+++ imageASCII.js
@@ -1,44 +1,43 @@
 const Jimp = require('jimp');
 
-function convertToAscii(imageBuffer) {
-    return new Promise (async (resolve, reject) => {
+async function convertToAscii(imageBuffer) {
+    try {
+        const image = await Jimp.read(imageBuffer);
 
-        try {
-            const image = await Jimp.read(imageBuffer);
+        // Resize the image to a suitable width
+        const width = 300; 
+        image.resize(width, Jimp.AUTO);
 
-            // Resize the image to a suitable width
-            const width = 200; // Adjust the width as needed
-            image.resize(width, Jimp.AUTO);
+        let asciiArt = '';
 
-            let asciiArt = '';
+        // Define the ASCII characters to use
+        const asciiChars = ['@', '#', 'S', '^', '&', '~', '/', '-', '_', 'o', '%', '?', '*', '+', ';', ':', ',', '.'];
 
-            // Define the ASCII characters to use
-            const asciiChars = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.'];
+        // Loop through each pixel in the resized image and map it to an ASCII character
+        image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+            const red = image.bitmap.data[idx];
+            const green = image.bitmap.data[idx + 1];
+            const blue = image.bitmap.data[idx + 2];
 
-            // Loop through each pixel in the resized image and map it to an ASCII character
-            image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
-                const red = image.bitmap.data[idx];
-                const green = image.bitmap.data[idx + 1];
-                const blue = image.bitmap.data[idx + 2];
+            const grayscale = (red + green + blue) / 3;
 
-                const grayscale = (red + green + blue) / 3;
+            const asciiIndex = Math.floor((grayscale / 255) * (asciiChars.length));
+            asciiArt += asciiChars[asciiIndex]; // Choose the char by the rounded result of the gray tone of the color multiplied by the lenght of the char array
 
-                const asciiIndex = Math.floor((grayscale / 255) * (asciiChars.length - 1));
-                asciiArt += asciiChars[asciiIndex];//choose the char by the result of the gray tone of the color multiplied by the lenght of the char array minus 
+            if (x === image.bitmap.width - 1) {
+                asciiArt += '\n'; // Start a new line for each row
+            }
+        });
+        return asciiArt;
 
-                if (x === image.bitmap.width - 1) {
-                    asciiArt += '\n'; // Start a new line for each row
-                }
-            });
+    } catch (error) {
+        console.error('Error converting image to ASCII:', error);
+        return error; ''; 
 
-            resolve (asciiArt);
-        } catch (error) {
-            console.error('Error converting image to ASCII:', error);
-            reject(error); ''; // Return an empty string on error
-        };
-    });
+    };
+};
 
-}
+
 
 
     
